5c8f65adbe18983b84d283eb18566cc2c6355403
userTOP04 <112961367+userTOP04@users.noreply.github.com>
2024-03-26 17:49:25 +0300

    Add files via upload

---
+++ olp2.py
@@ -0,0 +1,7 @@
+Text1 = input('Введите текст 1 ')
+Text2 = input('Введите текст 2 ')
+
+if Text1 == Text2:
+    print(True)
+else:
+    print(False)
---
+++ olp4.py
@@ -0,0 +1,11 @@
+a = input('Введите коэффициент a ')
+b = input('Введите коэффициент b ')
+c = input('Введите коэффициент c ')
+a = int(a)
+b = int(b)
+c = int(c)
+
+
+D = (b * b) - (4 * a * c)
+kD = D ** 0.5
+print(int(kD))
