0b8e391724d9c2575a49df2c473849535d5f35a6
userTOP04 <112961367+userTOP04@users.noreply.github.com>
2024-03-28 19:26:50 +0300

    Add files via upload

--- olp4.py
+++ olp4.py
@@ -1,11 +1,22 @@
-a = input('Введите коэффициент a ')
-b = input('Введите коэффициент b ')
-c = input('Введите коэффициент c ')
-a = int(a)
-b = int(b)
-c = int(c)
+def calculates_the_equation():
+	a = input('Введите коэффициент a ')
+	b = input('Введите коэффициент b ')
+	c = input('Введите коэффициент c ')
+	a = int(a)
+	b = int(b)
+	c = int(c)
 
 
-D = (b * b) - (4 * a * c)
-kD = D ** 0.5
-print(int(kD))
+	D = (b * b) - (4 * a * c)
+	kD = D ** 0.5
+	if D < 0:
+		return print('Корней нет')
+	elif D == 0:
+		x1 = ((-1) * b) / (2 * a)
+		return print(f'Корни квадратного уравнения: {x1}')
+	else:
+		x1 = ((-1) * b + kD) / (2 * a)
+		x2 = ((-1) * b - kD) / (2 * a)
+		return print(f'Корни квадратного уравнения: {x1} и {x2}')
+
+calculates_the_equation()
